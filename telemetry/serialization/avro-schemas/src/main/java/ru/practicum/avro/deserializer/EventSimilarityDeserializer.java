46f654fd9d456ff39d69dbdd9dd87dfe0c563c23
BelovEvgenii <Zhekab777@yandex.ru>
2026-08-14 14:32:24 +0300

    refactor: переделать сервис serialization, настроить помники

--- telemetry/serialization/avro-schemas/src/main/java/ru/practicum/avro/deserializer/EventSimilarityDeserializer.java
+++ telemetry/serialization/avro-schemas/src/main/java/ru/practicum/avro/deserializer/EventSimilarityDeserializer.java
@@ -2,9 +2,9 @@ package ru.practicum.avro.deserializer;
 
 
 import ru.practicum.ewm.stats.avro.EventSimilarityAvro;
-import ru.practicum.ewm.stats.avro.UserActionAvro;
 
-public class EventSimilarityDeserializer extends AvroDeserializer<EventSimilarityAvro> {
+public class EventSimilarityDeserializer  extends BaseAvroDeserializer<EventSimilarityAvro>{
     public EventSimilarityDeserializer() {
+        super(EventSimilarityAvro.getClassSchema());
     }
 }
